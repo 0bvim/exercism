e12d2906097355edcef01c32719beacf1ec87e1e
nivicius <niviciusdev@gmail.com>
2024-06-28 23:27:38 -0300

    feat: secrets
    in this I learn about anonymous functions

--- elixir/secrets/lib/secrets.ex
+++ elixir/secrets/lib/secrets.ex
@@ -1,29 +1,37 @@
 defmodule Secrets do
+  require Bitwise
   def secret_add(secret) do
-    add = fn secret -> secret + 1 end
+    fn(x) -> x + secret end
   end
 
   def secret_subtract(secret) do
-    # Please implement the secret_subtract/1 function
+    fn(x) -> x - secret end
   end
 
   def secret_multiply(secret) do
+    fn(x) -> secret * x end
     # Please implement the secret_multiply/1 function
   end
 
   def secret_divide(secret) do
-    # Please implement the secret_divide/1 function
+    fn(x) -> floor(x/secret) end
   end
 
   def secret_and(secret) do
-    # Please implement the secret_and/1 function
+    fn value -> Bitwise.band(value, secret) end
   end
 
   def secret_xor(secret) do
-    # Please implement the secret_xor/1 function
+    fn(value) -> Bitwise.bxor(value, secret) end
   end
 
   def secret_combine(secret_function1, secret_function2) do
-    # Please implement the secret_combine/2 function
+    fn(value) ->
+      value
+      |> secret_function1.()
+      |> round()
+      |> secret_function2.()
+      |> round()
+      end
   end
 end
