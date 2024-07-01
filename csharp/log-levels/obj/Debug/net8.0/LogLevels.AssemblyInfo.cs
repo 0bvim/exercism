12eee6ea3a5483e3f16a575bb7dca4ed9162ee7e
nivicius <niviciusdev@gmail.com>
2024-07-01 00:58:27 -0300

    feat: started new exercice and clean old

--- csharp/log-levels/obj/Debug/net8.0/LogLevels.AssemblyInfo.cs
+++ csharp/log-levels/obj/Debug/net8.0/LogLevels.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LogLevels")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dbb246d3a93e90218cb2d15b628bd925e04cd3b8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("LogLevels")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LogLevels")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
